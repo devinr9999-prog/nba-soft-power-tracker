90bea5856931ceb547386fa6caa730c96e55bd98
devinr9999-prog <devinr9999@gmail.com>
2026-02-25 14:53:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import pandas as pd
 import streamlit as st
 import requests
 import trafilatura
+from google.oauth2.service_account import Credentials
 
 import gspread
 from gspread.exceptions import WorksheetNotFound
